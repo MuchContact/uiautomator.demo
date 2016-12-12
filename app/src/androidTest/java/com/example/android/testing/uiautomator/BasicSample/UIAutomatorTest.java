2d09db5b42b4bc624d575562b8182100f8f0cc2c
MuchContact <liuhengjiangis@foxmail.com>
2016-12-12 10:35:12 +0800

    uiautomator scaffold project

---
+++ app/src/androidTest/java/com/example/android/testing/uiautomator/BasicSample/UIAutomatorTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2015, The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.testing.uiautomator.BasicSample;
+
+import android.support.test.InstrumentationRegistry;
+import android.support.test.filters.SdkSuppress;
+import android.support.test.runner.AndroidJUnit4;
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * Basic sample for unbundled UiAutomator.
+ */
+@RunWith(AndroidJUnit4.class)
+@SdkSuppress(minSdkVersion = 18)
+public class UIAutomatorTest {
+    private static final String STRING_TO_BE_TYPED = "UiAutomator";
+    private static final String ESHOP_SAMPLE_PACKAGE = "com.example.muco.eshop";
+
+    private UiDevice mDevice;
+
+    @Before
+    public void startMainActivityFromHomeScreen() {
+        // Initialize UiDevice instance
+        mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+    }
+
+    @Test
+    public void testName() throws Exception {
+        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "username"))
+                .setText(STRING_TO_BE_TYPED);
+        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "login"))
+                .click();
+
+    }
+}
