0a1aae0aaf3e4696161106cf9cd3c80537d9a197
MuchContact <liuhengjiangis@foxmail.com>
2016-12-15 22:54:51 +0800

    launch app before running instrumented test; add comment for inputting password field

--- app/src/androidTest/java/com/example/android/testing/uiautomator/BasicSample/UIAutomatorTest.java
+++ app/src/androidTest/java/com/example/android/testing/uiautomator/BasicSample/UIAutomatorTest.java
@@ -16,15 +16,23 @@
 
 package com.example.android.testing.uiautomator.BasicSample;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.filters.SdkSuppress;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.uiautomator.By;
 import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.Until;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
 
 /**
  * Basic sample for unbundled UiAutomator.
@@ -33,7 +41,8 @@ import org.junit.runner.RunWith;
 @SdkSuppress(minSdkVersion = 18)
 public class UIAutomatorTest {
     private static final String STRING_TO_BE_TYPED = "UiAutomator";
-    private static final String ESHOP_SAMPLE_PACKAGE = "com.example.muco.eshop";
+    private static final String ESHOP_SAMPLE_PACKAGE = "com.example.android.testing.uiautomator.BasicSample";
+    private static final int LAUNCH_TIMEOUT = 20000;
 
     private UiDevice mDevice;
 
@@ -41,14 +50,49 @@ public class UIAutomatorTest {
     public void startMainActivityFromHomeScreen() {
         // Initialize UiDevice instance
         mDevice = UiDevice.getInstance(InstrumentationRegistry.getInstrumentation());
+        // Start from the home screen
+        mDevice.pressHome();
+
+        // Wait for launcher
+        final String launcherPackage = getLauncherPackageName();
+        assertThat(launcherPackage, notNullValue());
+        mDevice.wait(Until.hasObject(By.pkg(launcherPackage).depth(0)), LAUNCH_TIMEOUT);
+
+        // Launch the blueprint app
+        Context context = InstrumentationRegistry.getContext();
+        final Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(ESHOP_SAMPLE_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);    // Clear out any previous instances
+        context.startActivity(intent);
+
+        // Wait for the app to appear
+        mDevice.wait(Until.hasObject(By.pkg(ESHOP_SAMPLE_PACKAGE).depth(0)), LAUNCH_TIMEOUT);
     }
 
     @Test
     public void testName() throws Exception {
-        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "username"))
+        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "editTextUserInput"))
                 .setText(STRING_TO_BE_TYPED);
-        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "login"))
+        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "password"))
+                .setText(STRING_TO_BE_TYPED);
+        mDevice.findObject(By.res(ESHOP_SAMPLE_PACKAGE, "changeTextBt"))
                 .click();
 
+    }
+
+    /**
+     * Uses package manager to find the package name of the device launcher. Usually this package
+     * is "com.android.launcher" but can be different at times. This is a generic solution which
+     * works on all platforms.`
+     */
+    private String getLauncherPackageName() {
+        // Create launcher Intent
+        final Intent intent = new Intent(Intent.ACTION_MAIN);
+        intent.addCategory(Intent.CATEGORY_HOME);
+
+        // Use PackageManager to get the launcher package name
+        PackageManager pm = InstrumentationRegistry.getContext().getPackageManager();
+        ResolveInfo resolveInfo = pm.resolveActivity(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return resolveInfo.activityInfo.packageName;
     }
 }
